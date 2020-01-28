0d5a437e6dc650fa15e52018920fca9179f3dedc
kmbarnett <kmbarnett94@gmail.com>
2020-01-28 15:42:25 -0700

    Fix some wording in deck class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -12,8 +12,8 @@ describe('Deck', function() {
   });
 
   it('should be an instance of Deck', function() {
-    const card = new Deck();
-    expect(card).to.be.an.instanceof(Deck);
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
   });
 
   it('should be able to have a card', function() {
@@ -24,16 +24,16 @@ describe('Deck', function() {
 
   it('should be able to have a more cards', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
-    const card2 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
-    const card3 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+    const card2 = new Card(2, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+    const card3 = new Card(3, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
     const deck = new Deck([card1, card2, card3]);
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
 
   it('should be able to count the cards', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
-    const card2 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
-    const card3 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+    const card2 = new Card(2, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+    const card3 = new Card(3, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
     const deck = new Deck([card1, card2, card3]);
     expect(deck.countCards()).to.equal(3);
   });
