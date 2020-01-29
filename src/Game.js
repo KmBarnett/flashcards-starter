3303aae0a82ba453f1a462e2a8abb4a2929eae7e
kmbarnett <kmbarnett94@gmail.com>
2020-01-28 17:10:37 -0700

    Add passes all tests for start function

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,31 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null
+    this.deck = this.createDeck()
+  }
+
+  createDeck() {
+    // let cards = prototypeQuestions.map(card => card = new Card(card))
+    let cards = prototypeQuestions.map(card => {
+      card = new Card(card.id, card.question, card.answers, card.correctAnswer);
+      return card;
+    })
+    let deck = new Deck(cards)
+    return deck
+  }
+
+  start() {
+    this.currentRound = new Round(this.deck)
+    this.printMessage(this.currentRound.deck, this.currentRound)
+    this.printQuestion(this.currentRound)
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
